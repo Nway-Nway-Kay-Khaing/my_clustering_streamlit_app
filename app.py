c7f58b5499a50729f013b438ca672d8d90980c32
Nway-Nway-Kay-Khaing <6531501223@lamduan.mfu.ac.th>
2025-04-20 16:33:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,21 +9,15 @@ with open('kmeans_model.pkl','rb') as f:
     
     st.title(" k-Means Clustering Visualizer by Nway Nway Kay Khaing")
     
-    st.subheader(" Example Data for Visualization")
-    st.markdown("This demo uses example data (2D) to illustrate clustering results.")
-    
+   
     from sklearn.datasets import make_blobs
     X, _ =make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
     y_kmeans = loaded_model.predict(X)
 
 # Plotting
 fig, ax = plt.subplots()
-ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
-
-# Plot cluster centers
-centers = loaded_model.cluster_centers_
-ax.scatter(centers[:, 0], centers[:, 1], c='red', s=200, alpha=0.75, marker='X', label='Centroids')
+scatter = ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
+ax.scatter(loaded_model.cluster_centers_[:, 0],loaded_model.cluster_centers_[:, 1], s=300, c='red')
+ax.set_title('k-Means Clustering')
 ax.legend()
-
-# Display the plot in Streamlit
 st.pyplot(fig)

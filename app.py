094423aff2da753c3aba35acdd30a237d157e2f8
Nway-Nway-Kay-Khaing <6531501223@lamduan.mfu.ac.th>
2025-04-20 16:53:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
+#app.py
 import streamlit as st
 import pickle
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 
-with open('kmeans_model.pkl','rb') as f:
-    loaded_model = pickle.load(f)
+#Load model
+with open('kmeans_model.pkl', 'rb') as f:
+    loaded_model  = pickle.load(f)
     
-    st.set_page_config(page_title="k-Means Clustering App", layout="centered")
-    
-    st.title(" k-Means Clustering Visualizer by Nway Nway Kay Khaing")
-    
-   
-    from sklearn.datasets import make_blobs
-    X, _ =make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
-    y_kmeans = loaded_model.predict(X)
+#Set the page config
+st.set_page_config(page_title="k-Means Clustering App", layout="centered")
+
+#Set title
+st.title(" k-Means Clustering Visualizer By Nway Nway Kay Khaing")
+
+#Display cluster centers
+st.subheader(" Example Data for Visualization")
+st.markdown("This demo uses example data(2D) to illustrate clustering results.")
+
+#Load from a saved dataset ot generate synthetic data
+
+X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
+
+#Predictusing the loaded model
+y_kmeans=loaded_model.predict(X)
+
+
 
 # Plotting
-fig, ax = plt.subplots()
-scatter = ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
-ax.scatter(loaded_model.cluster_centers_[:, 0],loaded_model.cluster_centers_[:, 1], s=300, c='red')
+fig,ax = plt.subplots()
+scatter=ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
+ax.scatter(loaded_model.cluster_centers_[:, 0], loaded_model.cluster_centers_[:, 1], s=300, c='red', label='Centroids')
 ax.set_title('k-Means Clustering')
 ax.legend()
 st.pyplot(fig)
